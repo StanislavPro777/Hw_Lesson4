bb572e248fdc966f4e5093fed7f2c1666fe28f85
dn110795psv <stanislav.pidhola@privatbank.ua>
2022-01-17 20:01:21 +0200

    Hw_lesson2_task_1

---
+++ Lesson2/Lesson2_task_1.js
@@ -0,0 +1,16 @@
+/**
+ * Author: Pidgola Stanislav
+ */
+
+// ####Задача 1
+// Перепишите код, заменив оператор `if` на тернарный оператор `?`
+// var result;
+// if( 1 + 2 < 4)
+//     result = true;
+// else{
+//     result = false;
+// }
+
+var result;
+result = ( 1 + 2 < 4 ) ? true : false;
+console.log('result ',result);
